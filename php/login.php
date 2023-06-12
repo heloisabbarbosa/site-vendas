a1c48e0035a1633ec218e8738d9bc92932774f7a
Vinicius de Melo Silva <vinicius.msilva@hotmail.com>
2023-06-12 16:12:14 -0300

    teste3
    teste3
    
    teste2
    
    teste2

--- php/login.php
+++ php/login.php
@@ -30,13 +30,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nome = $_POST["nome"];
         $cpf = $_POST["cpf"];
 
-        $sql = "SELECT * FROM cadastros_parceiro WHERE cpf = '$cpf'";
+        $sql = "SELECT * FROM cadastros_parceiro WHERE cpf = '$cpf' AND email = '$email'";
         $result = mysqli_query($conn, $sql);
 
         // Verifica se a consulta retornou algum resultado
         if (mysqli_num_rows($result) > 0) {
             // Usuário já cadastrado
-            echo "CPF JÁ CADASTRADO";
+            echo "E-MAIL OU CPF JÁ CADASTRADO";
         } else {
             $sql_insert = "INSERT INTO cadastros_parceiro(nome, cpf, email, senha) VALUES ('$nome','$cpf','$email','$senha')";
             $result_insert = mysqli_query($conn, $sql_insert);
