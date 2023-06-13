6b9c9716793710471e2f7b3ecf60eda4156cf360
Vinicius de Melo Silva <vinicius.msilva@hotmail.com>
2023-06-12 22:22:05 -0300

    fim2.0
    fim2.0

--- ajax.js
+++ ajax.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     event.preventDefault();
 
     var formData = $(this).serialize(); // Serializa os dados do formulário
-
+    var minhaDiv = document.getElementById("conteudoLogin")
     $.ajax({
       url: "php/login.php", // Substitua pelo caminho correto para o arquivo PHP
       type: "POST",
@@ -15,7 +15,10 @@ $(document).ready(function () {
           window.location.href = "./profile.php";
         } else {
           // Exibe a mensagem de erro na div conteudoLogin
-          $("#conteudoLogin").text(response);
+          $("#conteudoLogin").empty(); // Limpa o conteúdo existente na div com o ID "conteudo"
+          var h3Element = $("<h2>").text(response); // Cria um elemento <h3> e define o texto como a resposta do arquivo PHP
+          $("#conteudoLogin").append(h3Element);
+          minhaDiv.style.color = "red";
         }
       },
       error: function () {
@@ -40,16 +43,19 @@ $(document).ready(function () {
           var h3Element = $("<h2>").text(response); // Cria um elemento <h3> e define o texto como a resposta do arquivo PHP
           $("#conteudoCadastro").append(h3Element);
           minhaDiv.style.color = "green";
+          console.log(response)
         }else{
           
           $("#conteudoCadastro").empty(); // Limpa o conteúdo existente na div com o ID "conteudo"
           var h3Element = $("<h2>").text(response); // Cria um elemento <h3> e define o texto como a resposta do arquivo PHP
           $("#conteudoCadastro").append(h3Element); // Exibe a resposta do arquivo PHP na div com o ID "conteudo"
           minhaDiv.style.color = "red";
+          console.log(response)
         }
         },
       error: function () {
         $("#conteudoCadastro").text("Erro ao enviar os dados."); // Mensagem de erro em caso de falha na requisição AJAX
+        console.log(response)
       }
     });
   });
--- php/login.php
+++ php/login.php
@@ -21,32 +21,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             echo "success";
         } else {
-            // Usuário não encontrado ou senha incorreta
-           echo "Email ou senha inválidos.";
+            // Verifica se o e-mail está cadastrado
+            $sql_check_email = "SELECT * FROM cadastros_parceiro WHERE email = '$email'";
+            $result_check_email = mysqli_query($conn, $sql_check_email);
+
+            // Verifica se o e-mail está cadastrado
+            $sql_check_senha = "SELECT * FROM cadastros_parceiro WHERE senha = '$senha'";
+            $result_check_senha = mysqli_query($conn, $sql_check_senha);
+
+            if (mysqli_num_rows($result_check_email) == 0) {
+                // E-mail não cadastrado
+                echo "E-MAIL NÃO CADASTRADO";
+            } elseif (mysqli_num_rows($result_check_senha) == 0) {
+                // Senha incorreta
+                echo "SENHA INCORRETA";
+            }
         }
+
     } else if ($formulario == "cadastro") {
         $email = $_POST["email"];
         $senha = $_POST["senha"];
         $nome = $_POST["nome"];
         $cpf = $_POST["cpf"];
 
-        $sql = "SELECT * FROM cadastros_parceiro WHERE cpf = '$cpf' AND email = '$email'";
-        $result = mysqli_query($conn, $sql);
+        // Verificar se o e-mail e o CPF já estão cadastrados
+        $sql_check = "SELECT * FROM cadastros_parceiro WHERE email = '$email' OR cpf = '$cpf'";
+        $result_check = mysqli_query($conn, $sql_check);
 
-        // Verifica se a consulta retornou algum resultado
-        if (mysqli_num_rows($result) > 0) {
-            // Usuário já cadastrado
-            echo "E-MAIL OU CPF JÁ CADASTRADO";
+        if (mysqli_num_rows($result_check) > 0) {
+            while ($row = mysqli_fetch_assoc($result_check)) {
+                if ($row['email'] == $email) {
+                    // E-mail já cadastrado
+                    echo "E-MAIL JÁ CADASTRADO";
+                    break;
+                }
+                if ($row['cpf'] == $cpf) {
+                    // CPF já cadastrado
+                    echo "CPF JÁ CADASTRADO";
+                    break;
+                }
+            }
         } else {
-            $sql_insert = "INSERT INTO cadastros_parceiro(nome, cpf, email, senha) VALUES ('$nome','$cpf','$email','$senha')";
+            // Realizar o cadastro
+            $sql_insert = "INSERT INTO cadastros_parceiro (nome, cpf, email, senha) VALUES ('$nome', '$cpf', '$email', '$senha')";
             $result_insert = mysqli_query($conn, $sql_insert);
-            
+
             if ($result_insert) {
                 echo "CADASTRO REALIZADO COM SUCESSO";
             } else {
                 echo "Erro ao cadastrar.";
             }
         }
-    }
+            }
 }
 ?>
